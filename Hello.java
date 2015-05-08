933a76438783a30c271dd392b5488a0d2698029e
Katherine Bernardez <katherinebernardez@katherines-mbp.lakesideschool.org>
2015-05-08 08:31:23 -0700

    Initial Version

---
+++ Hello.java
@@ -0,0 +1,13 @@
+public class Hello
+{
+   public static void main(String[] args)
+   {
+      System.out.println("Hello World!");
+      
+      for (int jj = 1; jj <= 2; jj++) 
+      { 
+         System.out.println("How many times does this print: "); 
+         System.out.println(jj); 
+      }
+   }
+}
